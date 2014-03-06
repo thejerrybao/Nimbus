2260d8cd05a85330fe3338e867cbc3306d124610
Jerry Bao <thejerrybao@gmail.com>
2014-03-06 00:00:57 -0800

    Added dbfunc.php

---
+++ admin/dbfunc.php
@@ -0,0 +1,16 @@
+<?php 
+/** Project Name: Nimbus (Report Form System)
+ ** Administration Backend (dbfunc.php)
+ **
+ ** Author: Jerry Bao (thejerrybao@gmail.com)
+ ** 2013-2014 CNH Circle K District Technology Chair
+ **
+ ** Author: Roberto Rodriguez (rob.v.rodriguez@gmail.com)
+ ** 2013-2014 UC Berkeley Circle K Technology Chair
+ ** 
+ ** CALIFORNIA-NEVADA-HAWAII DISTRICT
+ ** CIRCLE K INTERNATIONAL
+ ** COPYRIGHT 2013-2014 - ALL RIGHTS RESERVED
+ **/
+
+?>
