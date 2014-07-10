7d405a5156ce18fdf4c4e9d72a2536b31ad36792
Jerry Bao <thejerrybao@gmail.com>
2014-07-09 18:21:11 -0700

    Initial database class connection

--- admin/dbfunc.php
+++ admin/dbfunc.php
@@ -10,4 +10,27 @@
  ** COPYRIGHT 2014-2015 - ALL RIGHTS RESERVED
  **/
 
+// SQL Database Info
+define("MYSQL_HOST", "localhost");
+define("MYSQL_USER", "917615_ckirfdb");
+define("MYSQL_PASS", "ckirfdb123");
+define("MYSQL_DB", "ucbckirfsystem_zymichost_ckirfsystem");
+
+class DatabaseFunctions {
+
+    private static $db;
+
+    function __construct() {
+        try {
+            $this->$db = new PDO("mysql:host=" . MYSQL_HOST . ";dbname=" . MYSQL_DB, MYSQL_USER, MYSQL_PASS);
+        }
+        catch (PDOException $e) {
+            die($e->getMessage());
+        }
+
+        $this->$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $this->$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+}
+
 ?>
