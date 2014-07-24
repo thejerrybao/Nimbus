4dc81f6c6f656f8ad68c85dc645f435eb83d525e
robrodz <rob.v.rodriguez@gmail.com>
2014-07-23 17:54:14 -0700

    addTag and delteMRPLevel completed

--- admin/dbfunc.php
+++ admin/dbfunc.php
@@ -659,7 +659,17 @@ class DatabaseFunctions {
 
     // add mrp or mrf tag
     public function addTag($tagData) {
+        $query = $this->$db->prepare('INSERT INTO `tags`
+            VALUES ("", :name, :abbr, :auto_manage, :mrp_tag, :number, :active)')
 
+        if query->execute(array(
+            ':name' => $tagData['name'],
+            ':abbr' => $tagData['abbr'],
+            ':auto_manage' => $tagData['auto_manage'],
+            ':mrp_tag' => $tagData['mrp_tag'],
+            ':number' => $tagData['number'],
+            ':active' => $tagData['active'])) { return "Tag " . $tagData['name'] . " was successfully added!"; }
+        else { return "An error has occurred! Error: " . $dp->errorInfo(); }
     }
 
     // delete mrp or mrf tags; requires that no events have
@@ -674,7 +684,16 @@ class DatabaseFunctions {
 
     // delete mrp level
     public function deleteMRPlevel($level_ids) {
-        
+
+        foreach ($level_ids as $level) {
+            $query = $this->$db->prepare('DELETE FROM `mrp_levels`
+                WHERE level_id=:level_id');
+
+            if ($query->execute(array(
+                ':level_id' => $level_id,))) {
+                continue;
+            } else { return "An error has occurred! Error: " . $db->errorInfo(); }
+        }
     }
 
 }
