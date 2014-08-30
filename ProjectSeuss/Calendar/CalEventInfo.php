f87f60f59b0bfadd90505be4a3290d749f45c1b0
diyaraniwar <diyaraniwar@berkeley.edu>
2014-08-29 23:50:28 -0700

    signups need work but calendar done!

--- ProjectSeuss/Calendar/CalEventInfo.php
+++ ProjectSeuss/Calendar/CalEventInfo.php
@@ -4,6 +4,7 @@
           define('__ROOT__', dirname(dirname(dirname(__FILE__))));
           require_once(__ROOT__.'/admin/dbfunc.php'); 
           $eventdb = new EventFunctions;
+          $userdb = new UserFunctions;
           $id = $_GET["id"];
           $event = $eventdb->getEventInfo($id);
           echo $event['name']
@@ -18,13 +19,45 @@
       </div>
       <div class="modal-body">
           <label>
+            Chair
+          </label>
+            <p><?php $chair = $userdb->getUserInfo($event['chair_id']); echo $chair['first_name']; echo " "; echo $chair['last_name']; ?>  </p>
+          <label>
+            Start Time
+          </label>
+            <p> <?php echo date("F d, Y, h:i A",$event['start_datetime']); ?> <p>
+          <label>
+            End Time
+          </label>
+            <p> <?php echo date("F d, Y, h:i A",$event['end_datetime']); ?> <p>
+          <label>
             Description
           </label>
             <p> <?php echo $event['description'] ?> <p>
+          <label>
+            Location
+          </label>
+            <p> <?php echo $event['location'] ?> <p>
+          <label>
+            Meeting Location
+          </label>
+            <p> <?php echo $event['meeting_location'] ?> <p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <form action="/admin/processdata.php" method="post" enctype="multipart/form-data">
+          <input type="hidden" name="form_submit_type" value="add_event_attendees">
+          <input type="hidden" name="event_id" value="<?= $event['event_id'] ?>">
+          <select name="user_ids[]" id="form-add-event-attendees" class="form-control" multiple required>
+          <? $users = $userdb->getUsers("active"); ?>
+          <? $eventAttendeeIDs = $eventdb->getEventAttendees($event['event_id'], true); ?>
+          <? foreach ($users as $user) { ?>
+          <? if (!in_array($user['user_id'], $eventAttendeeIDs)) { ?>
+          <option value="<?= $user['user_id'] ?>"><?= $user['first_name'] ?> <?= $user['last_name'] ?></option>
+          <? } ?>
+          <? } ?>
+          </select>
+          <button type="submit" class="btn btn-primary" style="margin-top: 10px;">Add Attendees</button>
+          </form>       
       </div>
     </div>
   </div>
