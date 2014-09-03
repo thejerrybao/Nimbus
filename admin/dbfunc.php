de14f99edd1e180a8c63582c20d92a68c4dff7fc
Jerry Bao <thejerrybao@gmail.com>
2014-09-03 15:53:44 -0700

    styling fix

--- admin/dbfunc.php
+++ admin/dbfunc.php
@@ -1399,14 +1399,17 @@ class BlogFunctions extends Database {
 
         return $posts;
     }
+
     public function getRecentPosts($numstart, $numposts){
+
         $query = $this->db->prepare('SELECT * FROM `blog`
              ORDER BY `publish_datetime` DESC LIMIT :numstart, :numposts');
         $query->setFetchMode(PDO::FETCH_OBJ);
         $query->execute(array(
             ':numstart' => $numstart,
             ':numposts' => $numposts));
-       if ($query->rowCount() == 0) { return false; }
+        
+        if ($query->rowCount() == 0) { return false; }
         while ($row = $query->fetch()) {
             $posts[] = array(
                 'post_id' => $row->post_id,
