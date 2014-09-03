c18b2aa6fa01c366ec9f94bb52659317b8fb3755
Jerry Bao <thejerrybao@gmail.com>
2014-09-02 20:12:04 -0700

    one more time

--- admin/processdata.php
+++ admin/processdata.php
@@ -22,10 +22,10 @@ switch ($_POST['form_submit_type']) {
         $userData = $userdb->login($_POST['username'], $_POST['password']);
         if ($userData) {
             session_start();
-            $_SESSION['cki_rf_user_id'] = $userData['user_id'];
-            $_SESSION['cki_rf_access'] = $userData['access'];
-            $_SESSION['cki_rf_first_name'] = $userData['first_name'];
-            $_SESSION['cki_rf_last_name'] = $userData['last_name'];
+            $_SESSION['nimbus_user_id'] = $userData['user_id'];
+            $_SESSION['nimbus_access'] = $userData['access'];
+            $_SESSION['nimbus_first_name'] = $userData['first_name'];
+            $_SESSION['nimbus_last_name'] = $userData['last_name'];
             $location = 'Location: index.php';
         } else { echo "Incorrect username/password."; } 
         break;
