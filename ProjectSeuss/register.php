cab644f57aef1b17917c14e7a3084c9b6f9be16a
diyaraniwar <diyaraniwar@berkeley.edu>
2014-09-02 23:48:51 -0700

    registration

---
+++ ProjectSeuss/register.php
@@ -0,0 +1,22 @@
+<?php 
+        ini_set('display_errors', 1);
+        $path = $_SERVER['DOCUMENT_ROOT'];
+        $path .= "/admin/dbfunc.php";
+        include_once($path);
+
+        $userdb = new UserFunctions;
+        $userData = array(
+            "first_name" => $_POST['first_name'],
+            "last_name" => $_POST['last_name'],
+            "username" => $_POST['username'],
+            "password" => password_hash($_POST['password'], PASSWORD_BCRYPT),
+            "email" => $_POST['email'],
+            "phone" => $_POST['phone']);
+        if ($userdb->addUser($userData)) {
+            $message = "SUCCESS: " . $userData['first_name'] . " " . $userData['last_name'] . " was added to the database!";
+            setcookie("successmsg", $message, time()+3);
+        } else {
+            $message = "DATABASE ERROR: User could not be added!";
+            setcookie("errormsg", $message, time()+3);
+        }
+        $location = 'Location: /ProjectSeuss/index.php'; ?>
