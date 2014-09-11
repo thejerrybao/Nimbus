b2eb7ae26709d5c57fe976b4728d3518a429dd11
Jerry Bao <thejerrybao@gmail.com>
2014-09-11 02:25:03 -0700

    forgot that when you check the db for the last insert id of another function that inserts, its gonna be the wrong id for when you create a event and set its tags

--- admin/dbfunc.php
+++ admin/dbfunc.php
@@ -710,8 +710,9 @@ class EventFunctions extends Database {
             ':meeting_location' => $eventData['meeting_location'],
             ':online_signups' => $eventData['online_signups'],
             ':online_end_datetime' => date("Y-m-d H:i:00", $eventData['online_end_datetime'])))) {
-            $this->addEventAttendee($this->db->lastInsertId(), $eventData['chair_id']);
-            return $this->setEventTags($this->db->lastInsertId(), $tag_ids);
+            $event_id = $this->db->lastInsertId();
+            $this->addEventAttendee($event_id, $eventData['chair_id']);
+            return $this->setEventTags($event_id, $tag_ids);
         } else { return false; }
     }
 
